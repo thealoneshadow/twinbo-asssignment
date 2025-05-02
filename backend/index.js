756d18c50e5213a789e750cd3a231e98e06f5bed
thealoneshadow <amayauta12@gmail.com>
2025-05-03 00:15:21 +0530

    Backend Fixes

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 4000;
 
 // Middleware
 app.use(cors());
